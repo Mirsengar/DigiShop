2ff30b85823a205a69322a7ac8e20c2162247364
mirsengar <mirsengar@gmail.com>
2022-06-04 19:45:06 +0430

    first commit

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+namespace DigiShop;
+
+class Config {
+
+    public static $username   = "digishop";
+    public static $password   = "digishop";
+    public static $dbname     = "digishop";
+    public static $servername = "localhost";
+    public static $DEBUG_MODE = true;
+    public static $APP_NAME   = 'DigiShop';
+    public static $API_KEY_KAVENEGAR   = '';
+    public static $API_KEY_JWT   = 'fsgblkds;fhg;lgh;kadrhg;aekrugh;aekrgh;keargh;keaurgh;kearhg';
+
+
+
+
+
+
+}
---
+++ src/v1/controller/User.php
@@ -0,0 +1,64 @@
+<?php
+namespace DigiShop\Controller;
+
+use DigiShop\app;
+use DigiShop\Model\SMSModel;
+use DigiShop\Model\UserModel;
+use DigiShop\Utils\JWTUtils;
+
+class User
+{
+    private    $userModel ;
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
+    public function Login($json) {
+        if(!app::hasKeys($json, ['phoneNumber'])) {
+            app::out(['Data' => 'Wrong inputs' , 'MissingKeys' => app::missingKeys($json, ['phoneNumber'])]);
+        }
+        $userInstance = $this->userModel->getUserByPhoneNumber($json->phoneNumber);
+        $otpCode = app::generateRandomInt();
+        $smsModel = new SMSModel();
+        if($userInstance == null) {//user not exists
+            $this->userModel->addUserByPhoneNumber($json->phoneNumber, $otpCode);
+            $smsModel->sendSMS($json->phoneNumber, $otpCode , "LoginRequest");
+            app::out(["Result" => "Activation code sent by SMS", "OtpCode" => $otpCode] , HTTP_OK);
+        }
+        else {// user already exists
+            $this->userModel->updateOtpCode($json->phoneNumber, $otpCode);
+                $smsModel->sendSMS($json->phoneNumber, $otpCode , "LoginRequest");
+                app::out(["Result" => "Activation code sent by SMS", "OtpCode" => $otpCode] , HTTP_OK);
+        }
+    }
+
+    public function Activation($json) {
+        if(!app::hasKeys($json, ['phoneNumber', 'otpCode'])) {
+            app::out(['Data' => 'Wrong inputs' , 'MissingKeys' => app::missingKeys($json, ['phoneNumber', 'otpCode'])]);
+        }
+
+        if(!$this->userModel->getUserByPhoneNumber($json->phoneNumber)) {
+            app::out("phone number not exists");
+        }
+
+
+        if(!$this->userModel->checkOtpCode($json->phoneNumber, $json->otpCode)) {
+            app::out("otp code is wrong");
+        }
+
+        $this->userModel->resetOtpCode($json->phoneNumber);
+
+        $user = $this->userModel->getUserByPhoneNumber($json->phoneNumber);
+        $user['session'] = JWTUtils::encode(["userID" => $user['userID'], "phoneNumber" => $user['phoneNumber'] ]);
+
+        app::out($user , HTTP_OK);
+
+
+
+
+
+    }
+
+
+}
